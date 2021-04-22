ca10c970c11a4f02a56e6325977f2ee67f0c5986
glaucius <glaucius@gmail.com>
2021-04-22 16:09:53 +0000

    Added a s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" "glauciusjuniorpereira2021" {
   source  = "app.terraform.io/Glaucius-training/s3-bucket/aws"
   version = "1.15.0"
   # insert required variables here
-  bucket_prefix = "glauciusjuniorpereira"
+  # bucket_prefix = "glauciusjuniorpereira"
 }
